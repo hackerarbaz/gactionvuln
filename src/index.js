95a364e05c72fcff5008df301b6e803578df99ec
JSON", JSON : lol } <hackerarbaz@users.noreply.github.com>
2021-05-01 08:35:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default class Pencil {
-  constructor(durability = 50, length = 50, eraserDurability) {
+  constructor(durability = 150, length = 50, eraserDurability) {
     this.durability = durability;
     this.maxDurability = durability;
     this.length = length;
